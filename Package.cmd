28811d035e5144e4d2ffeeb4314c63e747406ab5
Martin Halliday <martin.bikeman@gmail.com>
2015-09-10 11:46:50 -0700

    Modified the title to make it easier to find in NuGet Package Manager

--- Package.cmd
+++ Package.cmd
@@ -1,1 +1,2 @@
 Tools\nuget.exe pack Prius.nuspec
+Tools\nuget.exe pack Prius.Mocks.nuspec
--- SharedVersion.cs
+++ SharedVersion.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.4.4")]
-[assembly: AssemblyFileVersion("1.0.4.4")]
+[assembly: AssemblyVersion("1.0.5.5")]
+[assembly: AssemblyFileVersion("1.0.5.5")]
